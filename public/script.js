36f20d92e7f142ade8e2438804535d571eaad529
Ben <wbpiggot@gmail.com>
2015-04-20 22:45:04 -0700

    Refactoring JavaScript functions

--- public/script.js
+++ public/script.js
@@ -33,7 +33,6 @@ $(document).ready(function() {
 var wayfinder = [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
 var map
 var map2
-var map3
 
 
 function initialize() {
@@ -55,7 +54,9 @@ function initialize() {
   google.maps.event.addDomListener(window, 'load', initialize);
 
 
-function initialize2(lat, lng) {
+
+function initialize2(lat,lng,markers) {
+
     var mapProp2 = {
     center: new google.maps.LatLng(lat, lng),
     zoom: 11,
@@ -69,24 +70,6 @@ function initialize2(lat, lng) {
     };
 
     map2 = new google.maps.Map(document.getElementById("googleMap2"), mapProp2);
-}
-
-
-function initialize3(lat,lng,markers) {
-
-    var mapProp2 = {
-    center: new google.maps.LatLng(lat, lng),
-    zoom: 11,
-    scrollwheel: false,
-    panControl: false,
-    zoomControl: true,
-    scaleControl: false,
-    streetViewControl: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: wayfinder
-    };
-
-    map2 = new google.maps.Map(document.getElementById("googleMap3"), mapProp2);
 
     var infowindow = new google.maps.InfoWindow({});
     var markersObj = {};
